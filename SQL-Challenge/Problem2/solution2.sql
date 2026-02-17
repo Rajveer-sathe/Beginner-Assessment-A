12abe177ba662ed083ec45d0b8eb4d88bbf1a71a
Rajveer-sathe <rajveer09sathe@gmail.com>
2026-02-17 13:07:45 +0530

    Refactor SQL query for salary calculations

--- SQL-Challenge/Problem2/solution2.sql
+++ SQL-Challenge/Problem2/solution2.sql
@@ -1,47 +1,25 @@
-WITH income_calc AS (
-    SELECT 
-        s.emp_id,
-        s.emp_name,
-        i.income,
-        (s.base_salary * i.percentage / 100) AS amount
-    FROM salary s
-    CROSS JOIN income i
-),
-deduction_calc AS (
-    SELECT 
-        s.emp_id,
-        s.emp_name,
-        d.deduction,
-        (s.base_salary * d.percentage / 100) AS amount
-    FROM salary s
-    CROSS JOIN deduction d
-)
-
 SELECT 
     s.emp_name AS EMPLOYEE,
 
-    -- Income Pivot
-    MAX(CASE WHEN ic.income = 'Basic' THEN ic.amount END) AS BASIC,
-    MAX(CASE WHEN ic.income = 'Allowance' THEN ic.amount END) AS ALLOWANCE,
-    MAX(CASE WHEN ic.income = 'Others' THEN ic.amount END) AS OTHERS,
+    -- Income
+    (SELECT s.base_salary * percentage/100 FROM income WHERE income='Basic') AS BASIC,
+    (SELECT s.base_salary * percentage/100 FROM income WHERE income='Allowance') AS ALLOWANCE,
+    (SELECT s.base_salary * percentage/100 FROM income WHERE income='Others') AS OTHERS,
 
-    -- Gross Salary
-    SUM(ic.amount) AS GROSS,
+    -- Gross
+    (SELECT SUM(s.base_salary * percentage/100) FROM income) AS GROSS,
 
-    -- Deduction Pivot
-    MAX(CASE WHEN dc.deduction = 'Insurance' THEN dc.amount END) AS INSURANCE,
-    MAX(CASE WHEN dc.deduction = 'Health' THEN dc.amount END) AS HEALTH,
-    MAX(CASE WHEN dc.deduction = 'House' THEN dc.amount END) AS HOUSE,
+    -- Deductions
+    (SELECT s.base_salary * percentage/100 FROM deduction WHERE deduction='Insurance') AS INSURANCE,
+    (SELECT s.base_salary * percentage/100 FROM deduction WHERE deduction='Health') AS HEALTH,
+    (SELECT s.base_salary * percentage/100 FROM deduction WHERE deduction='House') AS HOUSE,
 
-    -- Total Deductions
-    SUM(dc.amount) AS TOTAL_DEDUCTIONS,
+    -- Total Deduction
+    (SELECT SUM(s.base_salary * percentage/100) FROM deduction) AS TOTAL_DEDUCTIONS,
 
     -- Net Pay
-    SUM(ic.amount) - SUM(dc.amount) AS NET_PAY
+    (SELECT SUM(s.base_salary * percentage/100) FROM income) -
+    (SELECT SUM(s.base_salary * percentage/100) FROM deduction) AS NET_PAY
 
 FROM salary s
-JOIN income_calc ic ON s.emp_id = ic.emp_id
-JOIN deduction_calc dc ON s.emp_id = dc.emp_id
-
-GROUP BY s.emp_id, s.emp_name
 ORDER BY s.emp_name;
